5573a9d940409ad72b97142a17104d2f59ccfceb
Léo Duboin <34611103+d4ilyrun@users.noreply.github.com>
2022-08-20 18:09:54 +0200

    Add GitHub actions to run tests on push (#4)
    * [MISC]: Moved README
    
    * [BUILD][TESTS]: Add GTest CMakeLists.txt
    
    * [TESTS][CPU]: Add tests for registers
    
    * [CI]: Build and run tests
    
    * [CI][BUILD]: Add nix flakes
    
    * [CI]: Working test CI

---
+++ flake.nix
@@ -0,0 +1,85 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOs";
+      repo = "nixpkgs";
+      ref = "nixos-22.05";
+    };
+
+    flake-utils = {
+      type = "github";
+      owner = "numtide";
+      repo = "flake-utils";
+      ref = "master";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        stdenv = pkgs.multiStdenv;
+      in rec {
+
+        defaultPackage = packages.emu-gb;
+        
+        packages = let
+          nativeBuildInputs = with pkgs; [ gcc cmake ];
+        in {
+          emu-gb = stdenv.mkDerivation {
+            pname = "emu-gb";
+            version = "0.1.0";
+            src = self;
+
+            inherit nativeBuildInputs;
+
+            configurePhase = ''
+              mkdir build && cd build
+              cmake ..
+            '';
+
+            buildPhase = ''
+              make -j4 emu-gb
+            '';
+
+            installPhase = ''
+              mkdir -p $out/bin
+              mv emu-gb $out/bin
+            '';
+          };
+
+          unit-tests = stdenv.mkDerivation {
+            pname = "unit-tests";
+            version = "0.1.0";
+            src = self;
+
+            inherit nativeBuildInputs;
+            buildInputs = with pkgs; [ gtest ];
+
+            configurePhase = ''
+              mkdir build && cd build
+              cmake -DBUILD_TESTS=ON ..
+            '';
+
+            buildPhase = ''
+              make -j4
+            '';
+
+            installPhase = ''
+              mkdir -p $out/tests
+              mv tests/*_test $out/tests
+            '';
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          name = "emu-gb";
+          inputsFrom = [ packages.emu-gb ];
+          buildInputs = with pkgs; [ doxygen valgrind gtest ];
+        };
+      }
+    );
+}
---
+++ tests/README.md
@@ -0,0 +1,6 @@
+# TESTS
+
+## ROMs
+
+I've taken these ROM files from [rokytriton's Game Boy emulator](https://github.com/rockytriton/LLD_gbemu/tree/main/roms).  
+To use them it is necessary to compile with `-DTEST_ROM`. They should display wether the test passed (or run indefinitely ...).
