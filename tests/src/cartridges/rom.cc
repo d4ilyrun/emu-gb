87510468c31f047339b75d104b2b9040dd9690e1
Léo DUBOIN <leo@duboin.com>
2022-08-22 20:24:34 +0200

    [TESTS][CART]: Add tests for rom-only cartridges

---
+++ tests/src/cartridges/rom.cc
@@ -0,0 +1,61 @@
+#define REG_ERR REG_ERR_
+#include <gtest/gtest-death-test.h>
+#include <gtest/gtest-param-test.h>
+#include <gtest/gtest.h>
+#undef REG_ERR
+
+extern "C" {
+#include <CPU/memory.h>
+#include <cartridge/cartridge.h>
+#include <cartridge/memory.h>
+}
+
+#include "cartridge.hxx"
+
+namespace cartridge_tests
+{
+
+class ROMOnly : public CartridgeGenerator<1 << 15>,
+                public ::testing::TestWithParam<u16>
+{
+  public:
+    ROMOnly() : CartridgeGenerator(MBC1) {}
+
+    void SetUp()
+    {
+        // Manually set the newly generated cartridge as loaded
+        cartridge = this->cart_;
+    };
+};
+
+TEST_P(ROMOnly, Write)
+{
+    const auto &address = GetParam();
+    const auto value = 0x1F;
+
+    write_cartridge(address, value);
+
+    ASSERT_EQ(cartridge.rom[address], value);
+}
+
+TEST_P(ROMOnly, Read)
+{
+    const auto &address = GetParam();
+    const auto value = 0x1F;
+
+    cartridge.rom[address] = value;
+
+    ASSERT_EQ(read_cartridge(address), value);
+}
+
+INSTANTIATE_TEST_SUITE_P(Memory, ROMOnly, ::testing::Values(0, 0x7FFF, 0x001F));
+
+using ROMOnlyDeath = ROMOnly;
+
+TEST_F(ROMOnlyDeath, OutOfRange)
+{
+    ASSERT_DEATH(write_cartridge(0x8000, 0x69), "");
+    ASSERT_DEATH(read_cartridge(0x8000), "");
+};
+
+}; // namespace cartridge_tests
