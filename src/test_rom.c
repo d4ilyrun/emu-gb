3db4f6cd36c52d06dc07f336df66bf990e779b81
Léo DUBOIN <leo@duboin.com>
2022-08-30 21:50:13 +0200

    misc: only update blargg's message when something new happened

--- src/test_rom.c
+++ src/test_rom.c
@@ -1,24 +1,30 @@
 #include "test_rom.h"
 
-#include <stdio.h>
-
 #include "CPU/memory.h"
 #include "utils/log.h"
-#include "utils/macro.h"
 
 static char output[1024] = {0};
-static u16 ouput_size = 0;
+static u16 output_size = 0;
+
+#define TEST_CHECK 0xFF02
+#define TEST_VALUE 0xFF01
+
+#define CHECK_TRUE 0x81
 
 void test_rom_update()
 {
     if (read_memory(TEST_CHECK) == CHECK_TRUE) {
-        output[ouput_size++] = (char)read_memory(TEST_VALUE);
+        output[output_size++] = (char)read_memory(TEST_VALUE);
         write_memory(TEST_CHECK, 0);
     }
 }
 
 void test_rom_print()
 {
-    if (output[0])
+    static u16 last_output_size = 0;
+
+    if (output[0] && output_size > last_output_size) {
         log_info("test result: %s", output);
+        last_output_size = output_size;
+    }
 }
