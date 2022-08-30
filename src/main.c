25ccc6d5646df7710c43d0af5fc055a03b30b1d5
Léo DUBOIN <leo@duboin.com>
2022-08-30 18:12:17 +0200

    misc: parse options at startup

--- src/main.c
+++ src/main.c
@@ -5,18 +5,16 @@
 #include "CPU/interrupt.h"
 #include "CPU/timer.h"
 #include "cartridge/cartridge.h"
+#include "options.h"
 #include "test_rom.h"
 #include "utils/log.h"
 #include "utils/macro.h"
 
 int main(int argc, char **argv)
 {
-    if (argc < 1) {
-        fputs("No cartridge provided.", stderr);
-        return 1;
-    }
+    const struct options *options = parse_options(argc, argv);
 
-    load_cartridge(argv[1]);
+    load_cartridge(options->args[0]);
     cartridge_info();
 
     reset_cpu();
---
+++ src/options.c
@@ -0,0 +1,88 @@
+/**
+ * \file options.c
+ * \creator Léo DUBOIN <leo@duboin.com>
+ *
+ * Parse the program's CLI arguments using glibc's argp library.
+ *
+ * Documentation: https://girishjoshi.io/post/glibc-argument-parsing-argp/
+ */
+
+#include "options.h"
+
+#include <argp.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <string.h>
+
+inline struct options *get_options(void)
+{
+    static struct options options = {
+        .log_level = LOG_INFO,
+        .trace = false,
+    };
+
+    return &options;
+}
+
+#define STR_EQ(str1, str2) !strcmp((str1), (str2))
+
+static error_t parse_opt(int key, char *value, struct argp_state *state)
+{
+    struct options *arguments = state->input;
+
+    switch (key) {
+    case 't':
+        arguments->trace = true;
+        break;
+
+    case 'l':
+        if (STR_EQ(value, "TRACE"))
+            arguments->log_level = LOG_TRACE;
+        else if (STR_EQ(value, "WARNING"))
+            arguments->log_level = LOG_WARNING;
+        else if (STR_EQ(value, "ERROR"))
+            arguments->log_level = LOG_ERROR;
+        else {
+            log_warn("Invalid argument for option -l / --log-level: %s", value);
+            arguments->log_level = LOG_INFO;
+        }
+        break;
+
+    case ARGP_KEY_ARG:
+        // Too many arguments, if your program expects only one argument.
+        if (state->arg_num > GBEMU_NB_ARGS)
+            argp_usage(state);
+        arguments->args[state->arg_num] = value;
+        break;
+
+    case ARGP_KEY_END:
+        if (state->arg_num < GBEMU_NB_ARGS)
+            argp_usage(state);
+        break;
+
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
+}
+
+static char doc[] = "gb-emu: yet another gameboy emulator written in C";
+static char args_doc[] = "CARTRIDGE";
+
+static struct argp_option long_options[] = {
+    // Log related
+    {"trace", 't', 0, 0, "Output traces during execution"},
+    {"log-level", 'l', "LEVEL", 0, "Do not output logs of lower importance"},
+
+    {0},
+};
+struct options *parse_options(int argc, char **argv)
+{
+    static struct argp argp = {long_options, parse_opt, args_doc, doc};
+    struct options *arguments = get_options();
+
+    argp_parse(&argp, argc, argv, 0, 0, arguments);
+
+    return get_options();
+}
