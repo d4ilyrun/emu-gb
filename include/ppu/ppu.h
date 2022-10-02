b0ee8393d452584ba7f81ac8648e39f994933700
Léo DUBOIN <leo@duboin.com>
2022-10-02 22:02:48 +0200

    PPU: Add write to TileMaps

--- include/ppu/ppu.h
+++ include/ppu/ppu.h
@@ -2,6 +2,41 @@
 
 #include "utils/types.h"
 
+/**
+ * \union oam_data
+ * \brief typed of the data contained inside the OAM memory
+ *
+ * \see https://gbdev.io/pandocs/OAM.html
+ */
+typedef union {
+
+    /// Fields with their actual meaning
+    struct oam_data {
+
+        u8 pos_y; ///< Sprite’s vertical position on the screen + 16
+        u8 pos_x; ///< Sprite’s horizontal position on the screen + 8
+        u8 tile_index;
+
+        /**
+         * __Bit7__   BG and Window over OBJ
+         *       __ (0=No, 1=BG and Window colors 1-3 over the OBJ) <br>
+         * __Bit6__   Y flip          (0=Normal, 1=Vertically mirrored) <br>
+         * __Bit5__   X flip          (0=Normal, 1=Horizontally mirrored) <br>
+         * __Bit4__   Palette number  **Non CGB Mode Only** (0=OBP0, 1=OBP1)
+         * <br>
+         * __Bit3__   Tile VRAM-Bank  **CGB Mode Only**     (0=Bank 0, 1=Bank
+         * 1)<br>
+         * __Bit2-0__ Palette number  **CGB Mode Only**     (OBP0-7) <br>
+         */
+        u8 flags;
+
+    } oam;
+
+    /// Raw representation (4 bytes)
+    u32 raw;
+
+} oam_data;
+
 struct ppu {
     /**
      * \brief The ppu's vram area for Tile data.
@@ -28,7 +63,7 @@ struct ppu {
      * Attribute Memory) at $FE00-FE9F. Each of the 40 entries consists of four
      * bytes with a specific meaning.
      */
-    u8 oam[0x80];
+    oam_data oam[40];
 };
 
 /**
